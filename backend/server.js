70f51649027c63ce75aff71e43a2bf11198e1507
preykriti <prakriteeshrestha2@gmail.com>
2025-11-26 10:36:20 +0545

    fix: configure backend url

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,16 @@ import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
-app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173'],
-  methods: ['GET','POST','PUT','DELETE'],
-  credentials: true,
-}));
-
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 //db connection
 connectDB();
@@ -36,8 +37,6 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 //middlewares
-
-app.use(cors());
 app.use(cookieParser());
 
 app.listen(PORT, () => {
